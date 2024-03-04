514b3373d3a7417001ea7d594248381ffea6181f
Kieu Huyen <hkieu716@gmail.com>
2024-03-04 21:47:16 +0700

    Topic 3 of chap6 - v0.0.1

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -1,5 +1,61 @@
 package commons;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BasePage {
+	
+	//Nhiệm vụ mở url ra		
+	public void openPageUrl(WebDriver driver, String pageUrl) {
+		driver.get(pageUrl);
+	}
+	
+	public String getPageTitle(WebDriver driver) {
+		return driver.getTitle();
+	}
+	
+	public String getPageUrl(WebDriver driver) {
+		return driver.getCurrentUrl();
+	}
+	
+	public String getPageSourceCode(WebDriver driver) {
+		return driver.getPageSource();
+	}
+	
+	public void backToPage(WebDriver driver) {
+		driver.navigate().back();
+	}
+	
+	public void forwardToPage(WebDriver driver) {
+		driver.navigate().forward();
+	}
+	
+	public void refreshCurrentPage(WebDriver driver) {
+		driver.navigate().refresh();
+	}
+	
+	public Alert waitForAlertPresence(WebDriver driver) {
+		WebDriverWait expliciWait = new WebDriverWait(driver, 30);
+		return expliciWait.until(ExpectedConditions.alertIsPresent());
+	}
+	
+	public void acceptAlert(WebDriver driver) {
+		Alert alert = waitForAlertPresence(driver);
+		alert.accept();
+	}
+	
+	public void cancelAlert(WebDriver driver) {
+		Alert alert = waitForAlertPresence(driver);
+		alert.dismiss();
+	}
+	
+	public String getAlertText(WebDriver driver) {
+		Alert alert = waitForAlertPresence(driver);
+		return alert.getText();
+		//video topic 3 59:41
+	}
+
 
 }
